91086d9bc8f36ba9ddb1fc9e55e08975e480fce2
Daniela Brenes <danibreo6@gmail.com>
2021-11-12 16:45:20 -0600

    Server en C

--- Server/Server.c
+++ Server/Server.c
@@ -3,37 +3,56 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netdb.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 int main() {
-
-    char server_message[256] = "Welcome to PaCEman";
 
     // Se crea el servidor
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (server_socket < 0) {
+        perror("Client Error: Socket not created succesfully");
+        return 0;
+    }
+
     // Se define la direccion del servidor
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(1201);
+    server_address.sin_addr.s_addr = htons(INADDR_ANY);
 
     // Se hace un bind del server al port y la IP
     bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-        
+
     // Esperar (listen) nuevas conexiones
-    listen(server_socket, 4);
+    listen(server_socket, 5);
 
-    // Aceptar conexiones
+    // Socket cliente
     int client_socket;
-    client_socket = accept(server_socket, NULL, NULL);
+    client_socket = accept(server_socket, (struct sockaddr*) NULL, NULL);
 
-    // Enviar mensaje inicial
-    send(client_socket, server_message, sizeof(server_message), 0);
+    // Mensaje cliente
+    char input[256];
+    char output[256];
 
-    // Cerrar socket servidor
+    while (1)
+    {
+        bzero(input, 256);
+        bzero(output, 256);
+
+        read(client_socket, input, 256);
+        printf("%s", input); // Mostrar mensaje del cliente
+
+        fgets(output, sizeof(output), stdin);
+        send(client_socket, output, sizeof(output), 0);
+
+    }
+    
     close(server_socket);
-
     return 0;
 }
