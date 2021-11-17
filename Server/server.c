206b46cc601878ac7233fcaa3422df89684187ea
Daniela Brenes <danibreo6@gmail.com>
2021-11-17 01:40:27 -0600

    Server comentado

--- Server/server.c
+++ Server/server.c
@@ -1,31 +1,37 @@
 #include "server.h"
+#include "/home/dani/Documents/GitHub/PaCEman/Server/variables.h"
 
+
+// Metodo que inicializa todo el servidor
 void init_server()
 {
     memset(clients_arr, 0, 4);
     current_client = 0;
-   // matrix_length = 168;
 
+    // Creacion del socket del servidor
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 
+    // Maneja errores de creacion del socket
     if (socket_desc == -1)
     {
         printf("No se pudo crear el socket");
     }
     puts("Socket creado");
 
+    // Establece direcciones del socket
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8080 );
+    server.sin_port = htons(PORT);
 
-
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    // Bind del socket al puerto y direccion deseados
+    if (bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
     {
-        perror("enlace fallido. Error");
+        perror("Enlace fallido. Error");
         return;
     }
     puts("Enlace hecho");
 
+    // Escuchar posibles conexiones
     listen(socket_desc , 3);
 
     c = sizeof(struct sockaddr_in);
@@ -34,16 +40,17 @@ void init_server()
     c = sizeof(struct sockaddr_in);
 }
 
-
-
+// Metodo para manejar al servidor en un thread
 void run()
 {
     pthread_t thread_id;
 
+    // Aceptar conexiones
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
         puts("Conexion aceptada");
 
+        // Thread para cada conexion especifica
         if( pthread_create(&thread_id, NULL,  connection_handler, (void*) &client_sock) < 0)
         {
             perror("No se pudo crear el thread ");
@@ -62,9 +69,10 @@ void run()
 }
 
 
+// Maneja cada conexion individualmente
 void *connection_handler(void *socket_desc)
 {
-    int sock = *(int *) socket_desc;
+    int sock = *(int*) socket_desc;
     int read_size;
     char message[50], client_message[256];
 
@@ -72,9 +80,10 @@ void *connection_handler(void *socket_desc)
     sprintf(message, "Eres el cliente numero %d \n", current_client);
     write(sock, message, strlen(message));
 
-
+    // Se lee constantemente para recibir nuevos mensajes
     while ((read_size = recv(sock, client_message, 256, 0)) > 0)
     {
+        // Mensajes devueltos a todos los clientes
         send_to_all(strcat(client_message, "\n"));
         bzero(client_message, 256);
     }
@@ -90,6 +99,7 @@ void *connection_handler(void *socket_desc)
     }
 }
 
+// Metodo para enviar un mensaje a todos los clientes conectados
 void send_to_all(char* message)
 {
     for (int i = 0; i < 4; ++i)
@@ -99,12 +109,13 @@ void send_to_all(char* message)
     }
 }
 
+// Se incluye el nuevo cliente a la lista de clientes
 void set_client(int* sock)
 {
     pthread_mutex_lock(&locker);
 
     struct client *ptr_one;
-    ptr_one = (struct client *)malloc(sizeof(struct client));
+    ptr_one = (struct client*) malloc(sizeof(struct client));
     ptr_one->sock = sock;
 
     clients_arr[current_client] = ptr_one;
--- Server/variables.h
+++ Server/variables.h
@@ -3,8 +3,10 @@
 
 #include <sys/types.h>
 
+extern int PORT = 8080;
+
 extern int current_client;
-extern int socket_desc , client_sock , c;
+extern int socket_desc, client_sock, c;
 extern struct sockaddr_in server , client;
 extern pthread_mutex_t locker;
 extern struct client* clients_arr[4];
